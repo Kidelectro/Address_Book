956db74abc13d5545fbf95c454a91da892ba7412
Marshall Myers <marshall.myers@mobiwm.com>
2015-12-04 15:58:44 -0500

    Add initial model and controller tests

--- test/controllers/contacts_controller_test.rb
+++ test/controllers/contacts_controller_test.rb
@@ -1,7 +1,60 @@
 require 'test_helper'
 
 class ContactsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @contact = contacts(:one)
+    @contact2 = contacts(:two)
+  end
+
+  def count_contacts
+    Contact.count
+  end
+
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:contacts)
+  end
+
+  test "should route to new" do
+    get :new
+    assert_response :success
+  end
+
+  test "should create a new contact" do
+    contact = Contact.new(first_name: @contact.first_name,
+                          last_name: @contact.last_name,
+                          birthday: @contact.birthday)
+    contact.save
+    assert contact.save
+  end
+
+  test "should render new if document can not be saved" do
+    post :create, contact:{first_name: @contact2.first_name}
+    assert_template :new
+  end
+
+  test "does edit route get called" do
+    get :edit, id: @contact.id
+    assert_response :success
+  end
+
+  test "should update a contact" do
+    contact = contacts(:one)
+    patch :update, contact:{first_name: "Bob"}, id: contact.id
+    contact.reload
+    assert_equal "Bob", contact.first_name
+  end
+
+  test "does show route get called" do
+    get :show, id: @contact.id
+    assert_response :success
+  end
+
+  test "does a contact get destroyed on delete" do
+    post_delete = count_contacts - 1
+    contact = contacts(:two)
+    delete :destroy, id: contact.id
+    assert_equal count_contacts, post_delete
+  end
 end
--- test/models/contact_test.rb
+++ test/models/contact_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
-
+require 'pry'
 class ContactTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test 'validates that first_name is present' do
+    contact = Contact.new(last_name: "Solo", birthday: "1975-10-20")
+    contact.valid?
+    assert contact.errors.messages[:first_name].include?("can't be blank")
+  end
+
+  test 'validates that last_name is present' do
+    contact = Contact.new(first_name: "Han", birthday: "1975-10-20")
+    contact.valid?
+    assert contact.errors.messages[:last_name].include?("can't be blank")
+  end
+
+  test 'validates that birthday is present' do
+    contact = Contact.new(first_name: "Han", last_name: "Solo")
+    contact.valid?
+    assert contact.errors.messages[:birthday].include?("can't be blank")
+  end
 end
